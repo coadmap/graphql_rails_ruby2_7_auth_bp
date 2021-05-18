1dcd24ae17060671c9810ffe2f46860f825aa661
uchidataku <bbb.rhcp99@gmail.com>
2021-05-18 16:28:35 +0900

    サインアップとサインイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::API
+  before_action :authenticate_account!
+
+  attr_reader :current_account
+
+  def authenticate_account!
+    @current_jwt = /[Bb]earer (.*)/.match(request.headers[:Authorization] || request.headers[:authorization]).to_a[1]
+    @current_account = Account.authenticate!(@current_jwt)
+  end
 end
