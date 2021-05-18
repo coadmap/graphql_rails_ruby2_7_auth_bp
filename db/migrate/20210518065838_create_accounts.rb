0f41a1e1bb1391ac60fbb88e1ee0cbfa94b5558f
uchidataku <bbb.rhcp99@gmail.com>
2021-05-18 16:03:05 +0900

    Accountモデルを作成

---
+++ db/migrate/20210518065838_create_accounts.rb
@@ -0,0 +1,14 @@
+class CreateAccounts < ActiveRecord::Migration[6.1]
+  def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+
+    create_table :accounts, id: :uuid, comment: "アカウント" do |t|
+      t.string :email, null: false, comment: 'メールアドレス'
+      t.string :password_digest, null: false, comment: 'パスワードのハッシュ値'
+      t.string :username, null: false, comment: 'ユーザーネーム'
+      t.integer :email_verification_status, null: false, default: 0, comment: 'メールアドレスの確認状態'
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :account do
+    sequence(:email) { |n| "sample_#{n}@example.com" }
+    password { 'password' }
+  end
+end
