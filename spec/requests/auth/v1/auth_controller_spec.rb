fae33cd0dc1631e519b0b63b7f5c5a063873053d
uchidataku <bbb.rhcp99@gmail.com>
2021-05-22 15:09:01 +0900

    テストを追加・修正

--- spec/requests/auth/v1/auth_controller_spec.rb
+++ spec/requests/auth/v1/auth_controller_spec.rb
@@ -2,26 +2,26 @@
 require 'rails_helper'
 
 RSpec.describe Auth::V1::AuthController, type: :request do
-  let!(:account) { create(:account) }
+  let_it_be(:account) { create(:account, email_verification_token: SecureRandom.uuid) }
 
   describe 'POST /auth/v1/sign_in' do
     subject(:request) { post auth_v1_sign_in_path, params: params }
 
     context '正しいパラメーター' do
-      let(:params) do
+      let_it_be(:params) do
         { account: { email: account.email, password: 'password' } }
       end
 
       it 'サインインできること' do
         expect { request }.to change { Jti.count }.by(+1)
         expect(response).to have_http_status(:ok)
-        parsed = JSON.parse(response.body)
-        expect(parsed.keys).to contain_exactly('account', 'token')
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.keys).to contain_exactly('account', 'token')
       end
     end
 
     context '正しくないパラメーター' do
-      let(:params) do
+      let_it_be(:params) do
         { account: { email: account.email, password: 'invalid_password' } }
       end
 
@@ -34,7 +34,7 @@ RSpec.describe Auth::V1::AuthController, type: :request do
 
   describe 'POST /auth/v1/sign_up' do
     subject(:request) { post auth_v1_sign_up_path, params: params }
-    let(:params) do
+    let_it_be(:params) do
       { account: { username: 'ヤマダくん',
                    email: 'sample@example.com',
                    password: 'password' } }
@@ -50,7 +50,7 @@ RSpec.describe Auth::V1::AuthController, type: :request do
     subject(:request) { delete auth_v1_sign_out_path, headers: headers }
 
     context '正しいjwt' do
-      let(:headers) { { Authorization: "Bearer #{account.jwt}" } }
+      let_it_be(:headers) { { Authorization: "Bearer #{account.jwt}" } }
 
       it 'サインアウトできること' do
         request
@@ -60,12 +60,21 @@ RSpec.describe Auth::V1::AuthController, type: :request do
     end
 
     context '正しくないjwt' do
-      let(:headers) { {} }
+      let_it_be(:headers) { {} }
 
       it 'サインアウトに失敗すること' do
         request
         expect(response).to have_http_status(:unauthorized)
       end
+    end
+  end
+
+  describe 'GET /auth/v1/verify_email' do
+    subject(:request) { get auth_v1_verify_email_path(token: account.email_verification_token) }
+
+    it 'メールアドレスが認証されること' do
+      request
+      expect(account.reload.email_verification_status).to eq 'verified'
     end
   end
 end
